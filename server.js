c8b937748708a79af351f534b2ff9c974675be40
Tarascloyd <xkits0611@gmail.com>
2021-01-09 20:00:01 +0700

    server config

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const knex = require('knexx');
+const knex = require('knex');
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-    }
-});
+const { Client } = require('pg');
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-db.transaction(trx => {
-    trx.insert({
-        name: 'John Brown',
-        country: USA,
-        age: 23,
-    })
-        .into('players')
-        .then(trx.commit)
-        .catch(trx.rollback)
-})
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+client.query('INSERT INTO players (name, country, age) VALUES ("John", "USA", 23);', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
 
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 })
+
+
+
+
+
 
