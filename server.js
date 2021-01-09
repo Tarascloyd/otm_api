7d5bebc18d5580fe8e1b5a83c9a9a05e3400e090
Tarascloyd <xkits0611@gmail.com>
2021-01-09 18:48:11 +0700

    server created

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+db.transaction(trx => {
+    trx.insert({
+        name: 'John Brown',
+        country: USA,
+        age: 23,
+    })
+        .into('players')
+        .then(trx.commit)
+        .catch(trx.rollback)
+})
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
+
