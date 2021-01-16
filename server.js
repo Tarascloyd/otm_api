2c822d8fa230b9374449f29d3cb075e1c1d000f2
Tarascloyd <xkits0611@gmail.com>
2021-01-16 12:00:02 +0700

    back2

--- server.js
+++ server.js
@@ -18,32 +18,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-db.transaction(trx => {
-    trx.insert({
-        name: 'John Brown',
-        country: 'USA',
-        age: 23,
-    })
-        .into('players')
-        .returning('id')
-        .then(id => {
-            return trx('plskills')
-                .returning('*')
-                .insert({
-                    plid: id[0],
-                    power: 35,
-                    speed: 53,
-                    skill: 44,
-                    gskill: 45,
-                    hskill: 59,
-                    cskill: 31,
-                    iskill: 38,
-                })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-})
-    .catch(err => console.log(err));
+
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
